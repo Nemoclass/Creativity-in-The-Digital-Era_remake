1e728934ebbb9c3f871e9f22b44a257aac459dfb
usss <88474883+sukimumuu@users.noreply.github.com>
2024-02-27 16:57:32 +0700

    admin

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/pamerangame', [PameranController::class, 'index'])->name('index');
+Route::get('/', [PameranController::class, 'index'])->name('index');
