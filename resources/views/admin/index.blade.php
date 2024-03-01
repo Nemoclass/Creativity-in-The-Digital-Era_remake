2feb6876286a63270d17f459c943afa982749cc5
usss <88474883+sukimumuu@users.noreply.github.com>
2024-03-01 13:09:32 +0700

    add table

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -158,6 +158,35 @@
               </form>
           </div>
         </div>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
     </div>
     <script src="{{ asset('admin/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('admin/jquery3.4.6.js') }}"></script>
