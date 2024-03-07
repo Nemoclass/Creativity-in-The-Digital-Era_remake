ea0e3c2efc505e726d0743a6c203f7b29a2d330d
vindrayulian <vindrayulian@gmail.com>
2024-03-07 12:42:49 +0700

    delete asset

--- public/javascript/game/main.js
+++ public/javascript/game/main.js
@@ -51,9 +51,30 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 //     },
 //   });
 
+// var swiper = new Swiper(".new-Swiper", {
+//     effect: "cards",
+//     grabCursor: true,
+//   });
+
 var swiper = new Swiper(".new-Swiper", {
-    effect: "cards",
+    effect: "coverflow",
     grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
   });
 
 var swiper = new Swiper(".team-Swiper", {
